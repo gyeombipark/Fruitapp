02f202224d7634e9657aff487b60b741d11b854f
bee00 <bee00@172.17.151.140>
2021-10-01 23:15:08 +0900

    Test

---
+++ Lab2/src/comc/example/lab2/Student.java
@@ -0,0 +1,41 @@
+package comc.example.lab2;
+
+public class Student {
+	
+	int no;
+	String name;
+	int age;
+	
+	public Student(int no, String name, int age) {}
+		
+    public int getNo() { return no; }
+    
+    public void setNo(int no) {}
+    
+    public String getName() { return name; }
+    
+    public void setName(String name) {}
+    
+    public int getAge() {return age;}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
